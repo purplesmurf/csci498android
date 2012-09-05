0ccc04694113a58555c5623d247c75e60b3617b1
Dominick Condoleo <dcondole@mines.edu>
2012-09-05 10:40:56 -0600

    finished tutorial 2 and 3

---
+++ LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
@@ -0,0 +1,57 @@
+package csci498.dcondole.lunchlist;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class MainActivity extends Activity {
+	Restaurant r=new Restaurant();
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button save=(Button)findViewById(R.id.save);
+        
+        save.setOnClickListener(onSave);
+    }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			EditText name = (EditText)findViewById(R.id.name);
+			EditText address=(EditText)findViewById(R.id.addr);
+			
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+			
+			RadioGroup types=(RadioGroup)findViewById(R.id.types);
+			
+			switch (types.getCheckedRadioButtonId()){
+			case R.id.sit_down:
+				r.setType("sit_down");
+				break;
+			case R.id.take_out:
+				r.setType("take_out");
+				break;
+			case R.id.delivery:
+				r.setType("delivery");
+				break;
+			}
+			
+		}
+	};
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
