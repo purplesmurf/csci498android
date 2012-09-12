d9f043fc99c1ceaab26b714691e5a38802351b7e
Dominick Condoleo <dcondole@mines.edu>
2012-09-11 19:56:39 -0600

    APT 6 no extra credit

--- LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
+++ LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
@@ -2,31 +2,42 @@ package csci498.dcondole.lunchlist;
 
 import android.R.id;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
 	List<Restaurant> model=new ArrayList<Restaurant>();
-	ArrayAdapter<Restaurant> adapter=null;
+	ArrayAdapter<Restaurant> adapter = null;
+	EditText name = null;
+	EditText address = null;
+	RadioGroup types = null;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		name = (EditText)findViewById(R.id.name);
+		address = (EditText)findViewById(R.id.addr);
+		types = (RadioGroup)findViewById(R.id.types);
 
 		Button save=(Button)findViewById(R.id.save);
 
@@ -36,7 +47,25 @@ public class MainActivity extends Activity {
 
 		adapter=new RestaurantAdapter();
 		list.setAdapter(adapter);
+
+		TabHost.TabSpec spec=getTabHost().newTabSpec("tag1");
+		spec.setContent(R.id.restaurants);
+
+		spec.setIndicator("List", getResources()
+				.getDrawable(R.drawable.list));
+
+		getTabHost().addTab(spec);
+		spec=getTabHost().newTabSpec("tag2");
+		spec.setContent(R.id.details);
+		spec.setIndicator("Details", getResources()
+				.getDrawable(R.drawable.restaurant));
+
+		getTabHost().addTab(spec);
+		getTabHost().setCurrentTab(0);
+
+		list.setOnItemClickListener(onListClick);
 	}
+
 	private View.OnClickListener onSave=new View.OnClickListener() {
 		public void onClick(View v) {
 			Restaurant r=new Restaurant();
@@ -65,63 +94,89 @@ public class MainActivity extends Activity {
 		}
 	};
 
-
-	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
-		RestaurantAdapter(){
-			super(MainActivity.this,
-					android.R.layout.simple_list_item_1,
-					model);
-		}
-
-		public View getView(int position, View convertView,
-				ViewGroup parent){
-
-			View row = convertView;
-			RestaurantHolder holder = null;
+	private AdapterView.OnItemClickListener onListClick=new
+			AdapterView.OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent,
+				View view, int position,
+				long id) {
 			
-			if (row==null) {
-				LayoutInflater inflater=getLayoutInflater();
-				row=inflater.inflate(R.layout.row, parent, false);
-				holder=new RestaurantHolder(row);
-				row.setTag(holder);
-			}
-			else {
-				holder=(RestaurantHolder)row.getTag();
-			}
-			
-			holder.populateFrom(model.get(position));
-
-			return(row);
-
-		}
-
-	}
-
-
-	static class RestaurantHolder {
-		private TextView name = null;
-		private TextView address = null;
-		private ImageView icon = null;
-
-		RestaurantHolder(View row) {
-			name = (TextView)row.findViewById(R.id.title);
-			address = (TextView)row.findViewById(R.id.address);
-			icon = (ImageView)row.findViewById(R.id.icon);
-		}
-		void populateFrom(Restaurant r) {
+			Restaurant r=model.get(position);
 			name.setText(r.getName());
 			address.setText(r.getAddress());
+		
+		if (r.getType().equals("sit_down")) {
+			types.check(R.id.sit_down);
+		}
+		else if (r.getType().equals("take_out")) {
+			types.check(R.id.take_out);
+		}
+		else {
+			types.check(R.id.delivery);
+		}
+		
+		getTabHost().setCurrentTab(1);
+		
+	}
+};
 
-			if (r.getType().equals("sit_down")) {
-				icon.setImageResource(R.drawable.ball_red);
-			}
-			else if (r.getType().equals("take_out")) {
-				icon.setImageResource(R.drawable.ball_yellow);
-			}
-			else {
-				icon.setImageResource(R.drawable.ball_green);
-			}
+
+
+class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+	RestaurantAdapter(){
+		super(MainActivity.this,
+				android.R.layout.simple_list_item_1,
+				model);
+	}
+
+	public View getView(int position, View convertView,
+			ViewGroup parent){
+
+		View row = convertView;
+		RestaurantHolder holder = null;
+
+		if (row==null) {
+			LayoutInflater inflater=getLayoutInflater();
+			row=inflater.inflate(R.layout.row, parent, false);
+			holder=new RestaurantHolder(row);
+			row.setTag(holder);
+		}
+		else {
+			holder=(RestaurantHolder)row.getTag();
+		}
+
+		holder.populateFrom(model.get(position));
+
+		return(row);
+
+	}
+
+}
+
+
+static class RestaurantHolder {
+	private TextView name = null;
+	private TextView address = null;
+	private ImageView icon = null;
+
+	RestaurantHolder(View row) {
+		name = (TextView)row.findViewById(R.id.title);
+		address = (TextView)row.findViewById(R.id.address);
+		icon = (ImageView)row.findViewById(R.id.icon);
+	}
+	void populateFrom(Restaurant r) {
+		name.setText(r.getName());
+		address.setText(r.getAddress());
+
+		if (r.getType().equals("sit_down")) {
+			icon.setImageResource(R.drawable.ball_red);
+		}
+		else if (r.getType().equals("take_out")) {
+			icon.setImageResource(R.drawable.ball_yellow);
+		}
+		else {
+			icon.setImageResource(R.drawable.ball_green);
 		}
 	}
+}
 }
 
