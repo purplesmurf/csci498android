491d12204950bd83ba5b2cb5c90c7386cd584790
Dominick Condoleo <dcondole@mines.edu>
2012-09-17 08:28:35 -0600

    pt 2 of EC for APT 9 complete couldn't figure out what pt 1 was asking

--- LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
+++ LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
@@ -91,14 +91,14 @@ public class MainActivity extends TabActivity {
 	};
 
 	@Override
-	public void onPause() {
-		super.onPause();
+	public void onStop() {
+		super.onStop();
 		isActive.set(false);
 	}
 
 	@Override
-	public void onResume() {
-		super.onResume();
+	public void onStart() {
+		super.onStart();
 		isActive.set(true);
 
 		if (progress>0) {
