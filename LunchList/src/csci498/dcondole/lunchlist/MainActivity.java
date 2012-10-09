e002282959373a252123073c90757a2e9f99f5ae
Dominick Condoleo <dcondole@mines.edu>
2012-10-09 16:30:14 -0600

    added a second menu option that sorts the list by what order you pick

--- LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
+++ LunchList/src/csci498/dcondole/lunchlist/MainActivity.java
@@ -1,10 +1,12 @@
 package csci498.dcondole.lunchlist;
 
 import android.app.ListActivity;
+import android.content.SharedPreferences;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,21 +20,43 @@ import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
 	public final static String ID_EXTRA="apt.tutorial._ID";
-	Cursor model=null;
-	RestaurantAdapter adapter=null;
-	RestaurantHelper helper=null;
+	Cursor model = null;
+	RestaurantAdapter adapter = null;
+	RestaurantHelper helper = null;
+	SharedPreferences prefs = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		helper=new RestaurantHelper(this);
-		model=helper.getAll();
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		helper = new RestaurantHelper(this);
+		
+		initList();
+
+		prefs.registerOnSharedPreferenceChangeListener(prefListener);
+	}
+
+	private void initList(){
+		if(model != null){
+			stopManagingCursor(model);
+			model.close();
+		}
+		model = helper.getAll(prefs.getString("sort_order", "name"));
 		startManagingCursor(model);
-		adapter=new RestaurantAdapter(model);
+		adapter = new RestaurantAdapter(model);
 		setListAdapter(adapter);
 	}
+
+	private SharedPreferences.OnSharedPreferenceChangeListener prefListener = new SharedPreferences.OnSharedPreferenceChangeListener()
+	{
+		public void onSharedPreferenceChanged(SharedPreferences sharedPrefs, String key){
+			if(key.equals("sort_order")){
+				initList();
+			}
+		}
+	};
 
 	@Override
 	public void onDestroy() {
@@ -66,7 +90,7 @@ public class MainActivity extends ListActivity {
 		}
 		else if (item.getItemId() == R.id.prefs){
 			startActivity(new Intent(this, EditPreferences.class));
-			
+
 			return (true);
 		}
 
--- LunchList/src/csci498/dcondole/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/dcondole/lunchlist/RestaurantHelper.java
@@ -27,9 +27,9 @@ class RestaurantHelper extends SQLiteOpenHelper {
 		// version exists
 	}
 
-	public Cursor getAll() {
+	public Cursor getAll(String orderBy) {
 		return(getReadableDatabase()
-				.rawQuery("SELECT _id, name, address, type, notes FROM restaurants ORDER BY name",
+				.rawQuery("SELECT _id, name, address, type, notes FROM restaurants ORDER BY "+orderBy,
 						null));
 	}
 
