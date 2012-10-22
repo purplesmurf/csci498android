bac615beddb32f99511d97f9ff91195f6b4f92bb
Dominick Condoleo <dcondole@mines.edu>
2012-10-21 19:44:32 -0600

    Adding the rss feed (apt 15) and the landscape for detail form

--- LunchList/src/csci498/dcondole/lunchlist/DetailForm.java
+++ LunchList/src/csci498/dcondole/lunchlist/DetailForm.java
@@ -1,24 +1,54 @@
 package csci498.dcondole.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 
 public class DetailForm extends Activity{
 
-	EditText name = null;
-	EditText address = null;
-	EditText notes = null;
-	RadioGroup types = null;
-	RestaurantHelper helper = null;
-	String restaurantId = null;
+	EditText name;
+	EditText address;
+	EditText notes;
+	EditText feed;
+	RadioGroup types;
+	RestaurantHelper helper;
+	String restaurantId;
+
+
+
+	@Override
+	public void onSaveInstanceState( Bundle state ){
+		super.onSaveInstanceState(state);
+
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes" , notes.getText().toString());
+		state.putInt("type" ,types.getCheckedRadioButtonId());
+	}
+
+	@Override
+	public void onRestoreInstanceState( Bundle state ){
+		super.onRestoreInstanceState(state);
+
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -41,6 +71,37 @@ public class DetailForm extends Activity{
 		if (restaurantId != null){
 			load();
 		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		if(item.getItemId() == R.id.feed) {
+			if (isNetworkAvailable()) {
+				Intent i = new Intent(this, FeedActivity.class);
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			}
+			else {
+				Toast.makeText(this, "Sorry, the Internet is not available",Toast.LENGTH_LONG).show();
+			}
+			return(true);
+		}
+		return(super.onOptionsItemSelected(item));
+	}
+	
+	private boolean isNetworkAvailable() {
+		
+		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info=cm.getActiveNetworkInfo();
+		
+		return(info!=null);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		new MenuInflater(this).inflate(R.menu.detail_option, menu);
+
+		return(super.onCreateOptionsMenu(menu));
 	}
 
 	@Override
@@ -91,11 +152,11 @@ public class DetailForm extends Activity{
 
 			if (restaurantId == null) {
 				helper.insert(name.getText().toString(), address.getText().toString(),
-						type, notes.getText().toString());
+						type, notes.getText().toString(), feed.getText().toString());
 			}
 			else {
 				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), 
-						type, notes.getText().toString());
+						type, notes.getText().toString(), feed.getText().toString());
 			}
 
 			finish();
--- LunchList/src/csci498/dcondole/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/dcondole/lunchlist/RestaurantHelper.java
@@ -9,8 +9,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 
 class RestaurantHelper extends SQLiteOpenHelper {
-	private static final String DATABASE_NAME="lunchlist.db";
-	private static final int SCHEMA_VERSION=1;
+	private static final String DATABASE_NAME = "lunchlist.db";
+	private static final int SCHEMA_VERSION = 2;
 
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -18,43 +18,42 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT);");
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// no-op, since will not be called until 2nd schema
-		// version exists
+		db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
 	}
 
 	public Cursor getAll(String orderBy) {
 		return(getReadableDatabase()
-				.rawQuery("SELECT _id, name, address, type, notes FROM restaurants ORDER BY "+orderBy,
+				.rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants ORDER BY "+orderBy,
 						null));
 	}
 
 	public Cursor getById(String id) {
-		String[] args={id};
+		String[] args = { id };
 
 		return(getReadableDatabase()
-				.rawQuery("SELECT _id, name, address, type, notes FROM restaurants WHERE _ID=?",
+				.rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants WHERE _ID=?",
 						args));
 	}
 
-	public void insert(String name, String address,
-			String type, String notes) {
+	public void insert(String name, String address, String type, String notes, String feed) {
 		ContentValues cv=new ContentValues();
 
 		cv.put("name", name);
 		cv.put("address", address);
 		cv.put("type", type);
 		cv.put("notes", notes);
+		cv.put("feed", feed);
 
 		getWritableDatabase().insert("restaurants", "name", cv);
 	}
 
 	public void update(String id, String name, String address,
-			String type, String notes) {
+			String type, String notes, String feed) {
 		ContentValues cv=new ContentValues();
 		String[] args={id};
 
@@ -62,6 +61,7 @@ class RestaurantHelper extends SQLiteOpenHelper {
 		cv.put("address", address);
 		cv.put("type", type);
 		cv.put("notes", notes);
+		cv.put("feed",feed);
 
 		getWritableDatabase().update("restaurants", cv, "_ID=?",
 				args);
@@ -81,5 +81,9 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	public String getNotes(Cursor c) {
 		return(c.getString(4));
+	}
+	
+	public String getFeed(Cursor c){
+		return(c.getString(5));
 	}
 }
