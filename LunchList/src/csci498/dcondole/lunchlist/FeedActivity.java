ab7a09a49a37a1708a4a9327d5b1c3c0883801b0
Dominick Condoleo <dcondole@mines.edu>
2012-10-31 18:47:04 -0600

    adding an intent service as a replacement async task [completion of APT 16]

--- LunchList/src/csci498/dcondole/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/dcondole/lunchlist/FeedActivity.java
@@ -1,148 +1,142 @@
 package csci498.dcondole.lunchlist;
 
-import android.annotation.SuppressLint;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
+import org.mcsoxford.rss.RSSReader;
+
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.IntentService;
 import android.app.ListActivity;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import org.mcsoxford.rss.RSSItem;
-import org.mcsoxford.rss.RSSFeed;
-import org.mcsoxford.rss.RSSReader;
 
-@SuppressLint("NewApi")
 public class FeedActivity extends ListActivity {
-
-	public static final String FEED_URL="apt.tutorial.FEED_URL";
-	private InstanceState state=null;
-
+	public static final String FEED_URL = "csci498.dcondole.lunchlist.FEED_URL";
+	private InstanceState state = null;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		state=(InstanceState)getLastNonConfigurationInstance();
+		
+		state = (InstanceState)getLastNonConfigurationInstance();
+		
 		if (state == null) {
 			state = new InstanceState();
-			state.task=new FeedTask(this);
-			state.task.execute(getIntent().getStringExtra(FEED_URL));
-		}
-		else {
-			if (state.task!=null) {
-				state.task.attach(this);
+			state.handler = new FeedHandler(this);
+			
+			Intent i = new Intent(this, FeedService.class);
+			
+			i.putExtra(FeedService.EXTRA_URL, getIntent().getStringExtra(FEED_URL));
+			i.putExtra(FeedService.EXTRA_MESSENGER, new Messenger(state.handler));
+			
+			startService(i);
+		} else {
+			if (state.handler != null) {
+				state.handler.attach(this);
 			}
-			if (state.feed!=null) {
+			
+			if (state.feed != null) {
 				setFeed(state.feed);
 			}
 		}
 	}
-
+	
 	@Override
 	public Object onRetainNonConfigurationInstance() {
-		if (state.task!=null) {
-			state.task.detach();
+		if(state.handler != null){
+			state.handler.detach();
 		}
+		
 		return(state);
 	}
-
+	
 	private void setFeed(RSSFeed feed) {
-		state.feed=feed;
+		state.feed = feed;
 		setListAdapter(new FeedAdapter(feed));
 	}
-
+	
 	private void goBlooey(Throwable t) {
-		AlertDialog.Builder builder=new AlertDialog.Builder(this);
-		builder
-		.setTitle("Exception!")
-		.setMessage(t.toString())
-		.setPositiveButton("OK", null)
-		.show();
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		
+		builder.setTitle("Exception!").setMessage(t.toString()).setPositiveButton("OK", null).show();
 	}
-
+	
 	private static class InstanceState {
-		RSSFeed feed=null;
-		FeedTask task=null;
+		RSSFeed feed = null;
+		FeedHandler handler = null;
 	}
-
-	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
-
-		private RSSReader reader = new RSSReader();
-		private Exception e = null;
-		private FeedActivity activity = null;
-
-		FeedTask(FeedActivity activity) {
-			attach(activity);
-		}
-
-		void attach(FeedActivity activity) {
-			this.activity=activity;
-		}
-
-		void detach() {
-			this.activity=null;
-		}
-
-		@Override
-		public RSSFeed doInBackground(String... urls) {
-			RSSFeed result=null;
-			try {
-				result=reader.load(urls[0]);
-			}
-			catch (Exception e) {
-				this.e=e;
-			}
-			return(result);
-		}
-
-		@Override
-		public void onPostExecute(RSSFeed feed) {
-			if (e==null) {
-				activity.setFeed(feed);
-			}
-			else {
-				Log.e("LunchList", "Exception parsing feed", e);
-				activity.goBlooey(e);
-			}
-		}
-	}
-
+	
 	private class FeedAdapter extends BaseAdapter {
-
-		RSSFeed feed=null;
-
+		RSSFeed feed = null;
+		
 		FeedAdapter(RSSFeed feed) {
 			super();
-			this.feed=feed;
+			
+			this.feed = feed;
 		}
-
+		
 		public int getCount() {
 			return(feed.getItems().size());
 		}
-
+		
 		public Object getItem(int position) {
 			return(feed.getItems().get(position));
 		}
-
+		
 		public long getItemId(int position) {
 			return(position);
 		}
-
-		public View getView(int position, View convertView,
-				ViewGroup parent) {
-			View row=convertView;
-			if (row==null) {
-				LayoutInflater inflater=getLayoutInflater();
-				row=inflater.inflate(android.R.layout.simple_list_item_1,
-						parent, false);
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			
+			if (row == null) {		
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
 			}
-			RSSItem item=(RSSItem)getItem(position);
+			
+			RSSItem item = (RSSItem) getItem(position);
+			
 			((TextView)row).setText(item.getTitle());
+			
 			return(row);
 		}
 	}
+	
+	private static class FeedHandler extends Handler {
+		FeedActivity activity = null;
+		
+		FeedHandler(FeedActivity activity) {
+			attach(activity);
+		}
+		
+		void attach(FeedActivity activity) {
+			this.activity = activity;
+		}
+		
+		void detach() {
+			this.activity = null;
+		}
+		
+		@Override
+		public void handleMessage(Message msg) {
+			if (msg.arg1 == RESULT_OK) {
+				activity.setFeed((RSSFeed)msg.obj);
+			}
+			else {
+				activity.goBlooey((Exception)msg.obj);
+			}
+		}
+	}
 }
-
