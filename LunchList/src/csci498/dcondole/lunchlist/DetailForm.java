d3d2959794d3ac7d2508139d9df59ddb55fd18ef
Dominick Condoleo <dcondole@mines.edu>
2012-10-31 20:42:27 -0600

    APT 17 working!

--- LunchList/src/csci498/dcondole/lunchlist/DetailForm.java
+++ LunchList/src/csci498/dcondole/lunchlist/DetailForm.java
@@ -13,16 +13,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-
-public class DetailForm extends Activity{
-
+public class DetailForm extends Activity {
 	EditText name;
 	EditText address;
 	EditText notes;
@@ -34,92 +30,79 @@ public class DetailForm extends Activity{
 	LocationManager locMgr;
 
 	@Override
-	public void onPause() {
-		save();
-		locMgr.removeUpdates(onLocationChange);
-		
-		super.onPause();
-	}
-	
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		if (restaurantId == null) {
-			menu.findItem(R.id.location).setEnabled(false);
-		}
-		
-		return(super.onPrepareOptionsMenu(menu));
-	}
-
-	public void save() {
-		if (name.getText().toString().length()>0) {
-			String type = null;
-			switch (types.getCheckedRadioButtonId()) {
-			case R.id.sit_down:
-				type = "sit_down";
-				break;
-			case R.id.take_out:
-				type = "take_out";
-				break;
-			default:
-				type = "delivery";
-				break;
-			}
-			if (restaurantId == null) {
-				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
-			}
-			else {
-				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
-			}
-		}
-	}
-
-	@Override
-	public void onRestoreInstanceState( Bundle state ){
-		super.onRestoreInstanceState(state);
-
-		name.setText(state.getString("name"));
-		address.setText(state.getString("address"));
-		notes.setText(state.getString("notes"));
-		types.check(state.getInt("type"));
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_form);
 
-		helper = new RestaurantHelper(this);
+		locMgr=(LocationManager)getSystemService(LOCATION_SERVICE);
+		helper=new RestaurantHelper(this);
 
 		name = (EditText)findViewById(R.id.name);
 		address = (EditText)findViewById(R.id.addr);
 		notes = (EditText)findViewById(R.id.notes);
 		types = (RadioGroup)findViewById(R.id.types);
+		feed = (EditText)findViewById(R.id.feed);
 		location = (TextView)findViewById(R.id.location);
-		locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
 
 		restaurantId = getIntent().getStringExtra(MainActivity.ID_EXTRA);
 
-		if (restaurantId != null){
+		if (restaurantId!=null) {
 			load();
 		}
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		if(item.getItemId() == R.id.feed) {
+	public void onPause() {
+		save();
+		locMgr.removeUpdates(onLocationChange);
+
+		super.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		helper.close();
+
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.detail_option, menu);
+
+		return(super.onCreateOptionsMenu(menu));
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (restaurantId==null) {
+			menu.findItem(R.id.location).setEnabled(false);
+		}
+
+		return(super.onPrepareOptionsMenu(menu));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()==R.id.feed) {
 			if (isNetworkAvailable()) {
-				Intent i = new Intent(this, FeedActivity.class);
+				Intent i=new Intent(this, FeedActivity.class);
 
 				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
 				startActivity(i);
 			}
 			else {
-				Toast.makeText(this, "Sorry, the Internet is not available",Toast.LENGTH_LONG).show();
+				Toast
+				.makeText(this, "Sorry, the Internet is not available",
+						Toast.LENGTH_LONG)
+						.show();
 			}
+
 			return(true);
 		}
-		else if (item.getItemId() == R.id.location) {
-			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
+		else if (item.getItemId()==R.id.location) {
+			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+					0, 0, onLocationChange);
 
 			return(true);
 		}
@@ -127,14 +110,83 @@ public class DetailForm extends Activity{
 		return(super.onOptionsItemSelected(item));
 	}
 
-	LocationListener onLocationChange = new LocationListener() {
-		public void onLocationChanged(Location fix) {
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm=(ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info=cm.getActiveNetworkInfo();
 
-			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
-			location.setText(String.valueOf(fix.getLatitude()) + ", " + String.valueOf(fix.getLongitude()));
+		return(info!=null);
+	}
+
+	private void load() {
+		Cursor c=helper.getById(restaurantId);
+
+		c.moveToFirst();    
+		name.setText(helper.getName(c));
+		address.setText(helper.getAddress(c));
+		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
+
+		if (helper.getType(c).equals("sit_down")) {
+			types.check(R.id.sit_down);
+		}
+		else if (helper.getType(c).equals("take_out")) {
+			types.check(R.id.take_out);
+		}
+		else {
+			types.check(R.id.delivery);
+		}
+
+		location.setText(String.valueOf(helper.getLatitude(c))
+				+", "
+				+String.valueOf(helper.getLongitude(c)));
+
+		c.close();
+	}
+
+	private void save() {
+		if (name.getText().toString().length()>0) {
+			String type=null;
+
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				type="sit_down";
+				break;
+			case R.id.take_out:
+				type="take_out";
+				break;
+			default:
+				type="delivery";
+				break;
+			}
+
+			if (restaurantId==null) {
+				helper.insert(name.getText().toString(),
+						address.getText().toString(), type,
+						notes.getText().toString(),
+						feed.getText().toString());
+			}
+			else {
+				helper.update(restaurantId, name.getText().toString(),
+						address.getText().toString(), type,
+						notes.getText().toString(),
+						feed.getText().toString());
+			}
+		}
+	}
+
+	LocationListener onLocationChange=new LocationListener() {
+		public void onLocationChanged(Location fix) {
+			helper.updateLocation(restaurantId, fix.getLatitude(),
+					fix.getLongitude());
+			location.setText(String.valueOf(fix.getLatitude())
+					+", "
+							+String.valueOf(fix.getLongitude()));
 			locMgr.removeUpdates(onLocationChange);
 
-			Toast.makeText(DetailForm.this, "Location Saved", Toast.LENGTH_LONG).show();
+			Toast
+			.makeText(DetailForm.this, "Location saved",
+					Toast.LENGTH_LONG)
+					.show();
 		}
 
 		public void onProviderDisabled(String provider) {
@@ -144,86 +196,10 @@ public class DetailForm extends Activity{
 		public void onProviderEnabled(String provider) {
 			// required for interface, not used
 		}
-		
-		public void onStatusChanged(String provider, int status, Bundle extras) {
+
+		public void onStatusChanged(String provider, int status,
+				Bundle extras) {
 			// required for interface, not used
-		}
-	};
-
-	private boolean isNetworkAvailable() {
-
-		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
-		NetworkInfo info=cm.getActiveNetworkInfo();
-
-		return(info!=null);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
-		new MenuInflater(this).inflate(R.menu.detail_option, menu);
-
-		return(super.onCreateOptionsMenu(menu));
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-
-		helper.close();
-	}
-
-	private void load(){
-		Cursor c = helper.getById(restaurantId);
-
-		c.moveToFirst();
-		name.setText(helper.getName(c));
-		address.setText(helper.getAddress(c));
-		notes.setText(helper.getNotes(c));
-		feed.setText(helper.getFeed(c));
-
-		if (helper.getType(c).equals("sit_down")){
-			types.check(R.id.sit_down);
-		}
-		else if (helper.getType(c).equals("take_out")){
-			types.check(R.id.take_out);
-		}
-		else {
-			types.check(R.id.delivery);
-		}
-
-		location.setText(String.valueOf( helper.getLatitude(c)) + ", " + String.valueOf(helper.getLongitude(c)) );
-
-		c.close();
-	}
-
-	private View.OnClickListener onSave = new View.OnClickListener() {
-
-		@Override
-		public void onClick(View v) {
-			String type = null;
-
-			switch (types.getCheckedRadioButtonId()){
-			case R.id.sit_down:
-				type = "sit_down";
-				break;
-			case R.id.take_out:
-				type = "take_out";
-				break;
-			case R.id.delivery:
-				type = "delivery";
-				break;
-			}
-
-			if (restaurantId == null) {
-				helper.insert(name.getText().toString(), address.getText().toString(),
-						type, notes.getText().toString(), feed.getText().toString());
-			}
-			else {
-				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), 
-						type, notes.getText().toString(), feed.getText().toString());
-			}
-
-			finish();
 		}
 	};
 }
