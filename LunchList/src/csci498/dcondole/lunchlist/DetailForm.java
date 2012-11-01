634ad9ddb9db659d902639a4671c6840706ab373
Dominick Condoleo <dcondole@mines.edu>
2012-10-31 20:32:46 -0600

    added the gps to the lunch list

--- LunchList/src/csci498/dcondole/lunchlist/DetailForm.java
+++ LunchList/src/csci498/dcondole/lunchlist/DetailForm.java
@@ -3,6 +3,9 @@ package csci498.dcondole.lunchlist;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -27,17 +30,47 @@ public class DetailForm extends Activity{
 	RadioGroup types;
 	RestaurantHelper helper;
 	String restaurantId;
-
-
+	TextView location;
+	LocationManager locMgr;
 
 	@Override
-	public void onSaveInstanceState( Bundle state ){
-		super.onSaveInstanceState(state);
+	public void onPause() {
+		save();
+		locMgr.removeUpdates(onLocationChange);
+		
+		super.onPause();
+	}
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (restaurantId == null) {
+			menu.findItem(R.id.location).setEnabled(false);
+		}
+		
+		return(super.onPrepareOptionsMenu(menu));
+	}
 
-		state.putString("name", name.getText().toString());
-		state.putString("address", address.getText().toString());
-		state.putString("notes" , notes.getText().toString());
-		state.putInt("type" ,types.getCheckedRadioButtonId());
+	public void save() {
+		if (name.getText().toString().length()>0) {
+			String type = null;
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				type = "sit_down";
+				break;
+			case R.id.take_out:
+				type = "take_out";
+				break;
+			default:
+				type = "delivery";
+				break;
+			}
+			if (restaurantId == null) {
+				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
+			}
+			else {
+				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
+			}
+		}
 	}
 
 	@Override
@@ -61,10 +94,8 @@ public class DetailForm extends Activity{
 		address = (EditText)findViewById(R.id.addr);
 		notes = (EditText)findViewById(R.id.notes);
 		types = (RadioGroup)findViewById(R.id.types);
-
-		Button save = (Button)findViewById(R.id.save);
-
-		save.setOnClickListener(onSave);
+		location = (TextView)findViewById(R.id.location);
+		locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
 
 		restaurantId = getIntent().getStringExtra(MainActivity.ID_EXTRA);
 
@@ -78,6 +109,7 @@ public class DetailForm extends Activity{
 		if(item.getItemId() == R.id.feed) {
 			if (isNetworkAvailable()) {
 				Intent i = new Intent(this, FeedActivity.class);
+
 				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
 				startActivity(i);
 			}
@@ -86,14 +118,43 @@ public class DetailForm extends Activity{
 			}
 			return(true);
 		}
+		else if (item.getItemId() == R.id.location) {
+			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
+
+			return(true);
+		}
+
 		return(super.onOptionsItemSelected(item));
 	}
-	
-	private boolean isNetworkAvailable() {
+
+	LocationListener onLocationChange = new LocationListener() {
+		public void onLocationChanged(Location fix) {
+
+			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+			location.setText(String.valueOf(fix.getLatitude()) + ", " + String.valueOf(fix.getLongitude()));
+			locMgr.removeUpdates(onLocationChange);
+
+			Toast.makeText(DetailForm.this, "Location Saved", Toast.LENGTH_LONG).show();
+		}
+
+		public void onProviderDisabled(String provider) {
+			// required for interface, not used
+		}
+
+		public void onProviderEnabled(String provider) {
+			// required for interface, not used
+		}
 		
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// required for interface, not used
+		}
+	};
+
+	private boolean isNetworkAvailable() {
+
 		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
 		NetworkInfo info=cm.getActiveNetworkInfo();
-		
+
 		return(info!=null);
 	}
 
@@ -118,6 +179,7 @@ public class DetailForm extends Activity{
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 
 		if (helper.getType(c).equals("sit_down")){
 			types.check(R.id.sit_down);
@@ -128,6 +190,8 @@ public class DetailForm extends Activity{
 		else {
 			types.check(R.id.delivery);
 		}
+
+		location.setText(String.valueOf( helper.getLatitude(c)) + ", " + String.valueOf(helper.getLongitude(c)) );
 
 		c.close();
 	}
